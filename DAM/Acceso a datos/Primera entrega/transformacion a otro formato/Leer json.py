8d9e3cad39669ede0233dea9b40f39d1e6afb5af
Cat <137451009+catalinaherreram@users.noreply.github.com>
2024-12-08 20:16:47 +0100

    Delete DAM/Acceso a datos directory

--- DAM/Acceso a datos/Primera entrega/transformacion a otro formato/Leer json.py
+++
@@ -1,9 +0,0 @@
-import json
-import xml
-
-# abre el archivo cliente.json en modo lectura
-with open('cliente.json', 'r') as archivo:
-    datos = json.load(archivo)  # carga el contenido del archivo json en la variable datos
-
-# imprime los datos cargados en la consola
-print(datos)
